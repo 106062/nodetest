4e3391bcd9e9ff38a2b2143ec346b9d37e577729
Minato_KazeHaya <ryan.wang@tradevan.com.tw>
2021-02-17 15:00:46 +0800

    test: add fail test

--- test/functest.js
+++ test/functest.js
@@ -6,7 +6,11 @@ describe("function test add", function () {
 		assert.equal(func.doadd(1, 2), 3);
 	})
 
-	it("should add return 0 ", function () {
+	it("should add return 0", function () {
 		assert.equal(func.doadd("a", "b"), 0);
+	})
+	// error example
+	it("will return error", function () {
+		assert.equal(func.doadd(1, 2), 0);
 	})
 });
